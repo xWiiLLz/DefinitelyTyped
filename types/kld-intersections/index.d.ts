2d6f864267c3221431464fc87ff5bed2eb7547ed
William Bergeron-Drouin <william@inter-net.ca>
2019-11-05 13:36:25 -0500

    Added Arc static constructor

--- types/kld-intersections/index.d.ts
+++ types/kld-intersections/index.d.ts
@@ -22,11 +22,20 @@ export class Point2D {
     constructor(x: number, y: number);
 }
 
-export class ShapeInfo<T> {
+export class ShapeInfo<Name extends ShapeNames, T> {
     args: T;
-    name: ShapeNames;
+    name: Name;
 
     constructor(name: ShapeNames, args: T);
 
-    static line(p1x: number, p1y: number, p2x: number, p2y: number): ShapeInfo<[Point2D, Point2D]>;
+    static line(p1x: number, p1y: number, p2x: number, p2y: number): ShapeInfo<'Line', [Point2D, Point2D]>;
+
+    static arc(
+        centerX: number,
+        centerY: number,
+        radiusX: number,
+        radiusY: number,
+        startRadians: number,
+        endsRadians: number,
+    ): ShapeInfo<'Arc', [Point2D, number, number, number, number]>;
 }
